e2c436390d97db1c7b86add89ba212fa58dfb450
Zsolti37 <petozsolt33@gmail.com>
2016-10-06 12:30:31 +0200

    Add files via upload

---
+++ math_plus.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3.5
+# -*- coding: UTF-8 -*-
+
+import random, sys, shutil, os, time
+import winsound
+
+# Jó az eredmény - Hangjelzés
+freq = 1000
+dur = 250
+# Nemjó az eredmény - Hangjelzés
+freqn = 500
+durn = 300
+y = 4
+
+a = int()
+b = int()
+c = int()
+d = int()
+
+print ("-" * 80)
+print (" Szia Milán :-) ")
+print (" Gyakoljunk egy kis összeadást. ")
+print ("-" * 80)
+
+while True:
+    if a > 0:
+        if bal == jobb:
+            print("-" * 80)
+            print("A megadott szám jó !")
+            print ( bal, "=", jobb, )
+            print( a, " + ", "[", d, "]", " = ", b, " + ", c)
+            print("-" * 80)
+            #winsound.Beep(freq, dur)
+            #        winsound.PlaySound("*", winsound.SND_ALIAS)
+            #        time.sleep(0.5)
+            print("A Programból való kilépéshez nyomjd meg a CTRL-C billentyű kombinációt")
+        else:
+            print("-" * 80)
+            print("A megadott szám nemjó !")
+            print("A helyes szám =", "[", b + c - a, "]")
+            print(a, " + ", "[", b + c - a, "]", " = ", b, " + ", c)
+            print("-" * 80)
+            #winsound.Beep(freqn, durn)
+            #        winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+            #        time.sleep(0.5)
+            print("A Programból való kilépéshez nyomjd meg a CTRL-C billentyű kombinációt")
+    else:
+        pass
+
+    a = random.randrange(5, 15)
+    b = random.randrange(1, 9)
+    c = random.randrange(13, 20)
+
+    print ("-" * 80)
+    print (a, "+ [  ] = ", b, "+", c)
+    print ("-" * 80)
+
+    while True:
+        try:
+            d = int(input("Mennyi a hiányzó szám értéke? : "))
+#            if d < 0:
+#               d = 0
+#            else:
+#                pass
+#            z = y / d
+            break
+        except ValueError:
+            print("Ez nem egy szám, vagy nem egész érték lett megadva!")  # A belovasott érték valóban egész szám?
+#        except ZeroDivisionError:
+#            print("A megadott érték nulla vagy negatív!")  # A beolvasott érték nem lehet nulla
+        except KeyboardInterrupt:  # control-c -re kilép a programból
+            sys.exit(0)
+    bal = a + d
+    jobb = b + c
+    #
+    os.system('cls')
+#    if bal == jobb:
+#        print ("-" * 80)
+#        print ("A megadott szám jó !")
+#        print (a, " + ", "[", d, "]", " = ", b, " + ", c)
+#        print ("-" * 80)
+#        winsound.Beep(freq,dur)
+#        winsound.PlaySound("*", winsound.SND_ALIAS)
+#        time.sleep(0.5)
+#        print ("A Programból való kilépéshez nyomjd meg a CTRL-C billentyű kombinációt")
+#    else:
+#        print ("-" * 80)
+#        print ("A megadott szám nemjó !")
+#        print ("A helyes szám =", "[", b+c-a, "]" )
+#        print (a, " + ", "[", b+c-a, "]", " = ", b, " + ", c)
+#        print ("-" * 80)
+#        winsound.Beep(freqn,durn)
+#        winsound.PlaySound("SystemHand", winsound.SND_ALIAS)
+#        time.sleep(0.5)
+#        print ("A Programból való kilépéshez nyomjd meg a CTRL-C billentyű kombinációt")
